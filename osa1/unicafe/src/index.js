22120ab90ea058ba9d3381790c2c2f288328385e
Jari Poutanen <jj.poutanen@gmail.com>
2020-07-27 22:06:32 +0300

    Tehtävä 1.7 valmis

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -11,9 +11,26 @@ const Button = (props) => (
 
 )
 
+
+
+const Statistics = (props) => (
+
+  <div>
+    <Results text="Good" value={props.goodCount} />
+    <Results text="Neutral" value={props.neutralCount} />
+    <Results text="Bad" value={props.badCount} />
+    <Results text="All" value={props.allCount} />
+    <Results text="Average" value={props.avg}/>
+    <Results text="Positive" value={props.positivePctg}/>
+  </div>
+  
+)
+
 const Results = (props) => (
   <p style={{color: "grey",fontSize: "14pt"}}>{props.text} {props.value} </p>
 )
+
+
 
 const App = () => {
   // tallenna napit omaan tilaansa
@@ -21,8 +38,22 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  let clicks = good + neutral + bad;
   
-  
+  const countAvg = () => {
+    let goodPoints = good * 1;
+    let neutralPoints = neutral * 0;
+    let badPoints = bad * -1;
+    let avg = (goodPoints+neutralPoints+badPoints)/clicks;
+    return(avg)
+  }
+
+  const countPosPctg = () => {
+
+    return((good/clicks) * 100 + "%")
+
+  }
+
   return (
     <div>
       <h1 style={{color:"blue",textShadow:"grey 2px 2px 2px"}}>Give feedback</h1>
@@ -30,9 +61,7 @@ const App = () => {
       <Button  handleClick={() => setNeutral(neutral+1)}  text="Neutral" bgColor="yellow" margin="10px"/>
       <Button  handleClick={() => setBad(bad+1)} text="Bad" bgColor="red" margin="10px"/>
       <h1 style={{color:"blue",textShadow:"grey 2px 2px 2px"}}>Statistics</h1>
-      <Results text="Good" value={good} color="green"/>
-      <Results text="Neutral" value={neutral} color="yellow"/>
-      <Results text="Bad" value={bad} color="red"/>
+      <Statistics goodCount={good} neutralCount={neutral} badCount={bad} allCount={clicks} avg={countAvg()} positivePctg={countPosPctg()}/>
     </div>
   )
 }
